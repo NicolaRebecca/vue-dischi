0475392523a84f21381988d0346ed8017d056ef2
NicolaRebecca <master9895@gmail.com>
2021-07-15 17:47:52 +0200

    manca la ricerca per genere

---
+++ src/components/Album.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="album-container">
+        <div class="album-details">
+            <img :src="poster" alt="">
+            <h5>{{ title }}</h5>
+            <div class="author">{{ author }}</div>  
+            <div class="year">{{ year}}</div>     
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Album',
+    props: {
+        poster: String,
+        title: String,
+        author: String,
+        year: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+    .album-container {
+        text-align: center;
+        .album-details {
+            margin: 15px 10px;
+            background-color: rgb(48, 58, 68);
+            padding: 20px;
+            height: 390px;
+            
+            img {
+                width: 100%;
+            }
+            
+            h5 {
+                color: cadetblue;
+                padding: 15px 0;
+            }
+            
+            .author, .year {
+                color: rgb(110, 110, 110);
+            }
+        }
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container-fluid">
+        <div class="logo">
+            <img src="../assets/spotify-logo.png" alt="">
+        </div>
+        <div class="custom-select">
+            <select>
+                <option v-for="(album,index) in albums" :key="index">{{album.genre}}</option>
+            </select>
+            
+        </div>
+    </div>
+    
+</template>
+ 
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+.container-fluid {
+    background-color: rgb(46, 58, 70);
+    padding: 15px;
+        
+    img {
+        width: 60px;    
+    }
+}
+
+.custom-select{
+    display: flex;
+    margin: 0 auto;
+}
+</style>
